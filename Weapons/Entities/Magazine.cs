04a15761279c4aba7781ca791343501e66d30020
DaynerPow <daynerpowtube@gmail.com>
2026-05-09 22:11:31 +0300

    Додав Magazine.cs

---
+++ Weapons/Entities/Magazine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weapons.Entities
+{
+    internal class Magazine
+    {
+        public int Id;
+        public static int autoInc = 1;
+
+        public CaliberType Caliber;
+        public int bullets;
+
+
+    }
+}
