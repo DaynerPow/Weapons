7051f869bd08a02f6e01166f32d2e905cc3b3d8a
DaynerPow <daynerpowtube@gmail.com>
2026-05-09 22:13:35 +0300

    Додав CaliberTypes.cs

---
+++ Weapons/Types/CaliberTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weapons.Entities
+{
+    public enum CaliberType
+    {
+        Cal9mm,
+        Cal545mm,
+        Cal762mm,
+        Cal12Gauge
+    }
+}
